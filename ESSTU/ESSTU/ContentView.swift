a111840b2e13564546f32cc89a26d12c98d02b53
danilaeremin <danilka.eremin@gmail.com>
2022-09-17 21:29:37 +0800

    Треш

--- ESSTU/ESSTU/ContentView.swift
+++ ESSTU/ESSTU/ContentView.swift
@@ -9,14 +9,19 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
+    @State private var login: String = ""
     var body: some View {
-        Text(Greeting().greeting())
-            .padding()
+        
+       Text("")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 13")
+        
+            
+            
     }
 }
--- ESSTU/ESSTU/ESSTUApp.swift
+++ ESSTU/ESSTU/ESSTUApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ESSTUApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AuthScreenPattern()
         }
     }
 }
